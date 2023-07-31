d718422472b0369034a92b1a51505aea37fd6b99
dewank07 <dewankrastogi007@gmail.com>
2023-07-31 19:40:54 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./pages/home"
+import Customizer from "./pages/customizer"
+import Canvas from "./canvas"
+function App() {
+
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+
+    </main>
+  )
+}
+
+export default App
